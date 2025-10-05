d8183d621423ca93118ebdddb7efe218ef8c3c3b
KevinOkkotsu <knnguyen265@gmail.com>
2025-10-05 14:40:48 +0000

    task2_5_2 completed

---
+++ tasks/task2_5_2/Program.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val myAge = 29u
+    val universeAge = 13_800_000_000L
+    val status = 'M'
+    val name = "Sarah"
+    val height = 1.78f
+    val root2 = Math.sqrt(2.0)
+
+    println("My age is: $myAge")
+    println("My universeAge is: $universeAge")
+    println("My status is: $status")
+    println("My name is: $name")
+    println("My height is: $height")
+    println("The squareroot of 2 is is: $root2")
+}
