9a2e7d3974e90265a1073a7b5d3970df188d1d77
KevinOkkotsu <knnguyen265@gmail.com>
2025-10-09 14:38:21 +0000

    Completed task 3.3

---
+++ tasks/task3_2/Name.kt
@@ -0,0 +1,5 @@
+fun main() {
+    print("Enter your name: ")
+    val name = readln()
+    print("The length of your name is ${name.length}")
+}
--- tasks/task3_3/Conversion.kt
+++ tasks/task3_3/Conversion.kt
@@ -1,1 +1,8 @@
 // Task 3.3: conversion of strings to numbers
+fun main() {
+    print("What is your name: ")
+    val name = readln()
+
+    print("What is your age:")
+    val age = readln().toInt()
+}
