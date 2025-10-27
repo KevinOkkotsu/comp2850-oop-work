98e369a07646fc87726261f52785cee794d0ee41
KevinOkkotsu <knnguyen265@gmail.com>
2025-10-27 15:26:21 +0000

    Completed task 9

--- tasks/task9_6/src/Funcs.kt
+++ tasks/task9_6/src/Funcs.kt
@@ -1,5 +1,19 @@
 // Task 9.6: functions for dataset variance calculation
+//import java.io.File
 
 // Add variance() function here
+/*fun variance(dataset:List<Double>):Double {
+    (require(dataset.size>1) { "not enough data" })
+    val mean = dataset.average()
+    val difference = dataset.map { it-mean }.sumOf { it * it } // computes sum of modified set, the squared numbers
+    val variance = difference / (dataset.size - 1)
+    return variance
+}
 
 // Add readData() function here
+fun readData(filename: String) = buildList {
+    File(filename).forEachLine {
+        add(it.toDouble())
+    }
+}
+*/
--- tasks/task9_6/src/Main.kt
+++ tasks/task9_6/src/Main.kt
@@ -1,7 +1,55 @@
 // Task 9.6: application to compute dataset variance
 
 import kotlin.system.exitProcess
+import java.io.File
+import java.io.FileNotFoundException
 
-fun main(args: Array<String>) {
-    // Implement main program here
+fun main() {
+    var intList: List<Double> = emptyList()
+    
+    while (true) {
+        println("Enter a filename: (type exit to end)")
+        val input = readln().trim()
+        
+        if (input == "exit") {
+            if (intList.isNotEmpty()) {
+                try {
+                    println("Variance: ${variance(intList)}")
+                } catch (e: Exception) {
+                    println("Unable to calculate variance: ${e.message}")
+                }
+            } else {
+                println("No data loaded")
+            }
+            break
+        }
+        
+        try {
+            intList = readData(input)
+            println("Data points loaded: ${intList.size}")
+            println("Data: $intList")
+        } catch (e: FileNotFoundException) {
+            println("File cannot be found: $input")
+        } catch (e: NumberFormatException) {
+            println("File contains invalid number format. ${e.message}")
+        } catch (e: SecurityException) {
+            println("Permission to access file not granted: ${e.message}")
+        } catch (e: Exception) {
+            println("Error occurred: ${e.message}")
+        }
+    }
+}
+
+fun variance(dataset: List<Double>): Double {
+    require(dataset.size > 1) { "not enough data" }
+    val mean = dataset.average()
+    val difference = dataset.map { it - mean }.sumOf { it * it }
+    val variance = difference / (dataset.size - 1)
+    return variance
+}    
+
+fun readData(filename: String): List<Double> = buildList {
+    File(filename).forEachLine {
+        add(it.toDouble())
+    }
 }
